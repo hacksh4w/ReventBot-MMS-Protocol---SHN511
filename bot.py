c521eead59f968823b2c020221703bd9ebbe3742
hacksh4w <91671136+hacksh4w@users.noreply.github.com>
2022-11-05 23:03:20 +0530

    added command code

--- bot.py
+++ bot.py
@@ -31,35 +31,7 @@ async def on_message(message):
       return
     if message.content.startswith('$hello'):
       await message.channel.send('Hello! {}'.format(client.user))
-    
-    
-    # bot.py
-import os
-import discord
-import random
-from discord.ext import commands
-from discord.ext.commands import Bot
-from discord import Intents
-
-intents = Intents.all()
-from dotenv import load_dotenv
-
-load_dotenv('.env')
-TOKEN = os.getenv("DISCORD_TOKEN")
-GUILD = os.getenv("DISCORD_GUILD")
-intents=discord.Intents.default()
-
-client = discord.Client(intents=intents)
-intents.message_content = True
-
-@client.event
-async def on_ready():
-    guild = discord.utils.get(client.guilds, name='Skill Brigade')
-    print(
-        f'{client.user} is connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id})'
-    )
-
+ 
 #code to add event
 @client.event
 async def on_message(message):
