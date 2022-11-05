eabf4473fa8fae71e80432bfc904b72ade257ff2
hacksh4w <91671136+hacksh4w@users.noreply.github.com>
2022-11-05 21:06:01 +0530

    added .env

--- bot.py
+++ bot.py
@@ -1,22 +1,32 @@
 # bot.py
 import os
-
 import discord
+import random
 from discord.ext import commands
 from discord.ext.commands import Bot
 from discord import Intents
+#from keep_alive import keep_alive
+
 intents = Intents.all()
- #from dotenv import load_dotenv
-TOKEN = 'MTAzODQzMzkxNDUwNDYxMzk3OA.G48fmI.OshF3B7_3J7VRU-NDiiJlNUuPA8Pzc2hfriP0Q'
-#load_dotenv()
-#TOKEN = os.getenv('DISCORD_TOKEN')
-#GUILD = os.getenv('DISCORD_GUILD')
+from dotenv import load_dotenv
+
+load_dotenv('.env')
+TOKEN = os.getenv("DISCORD_TOKEN")
+GUILD = os.getenv("DISCORD_GUILD")
 
 client = discord.Client(intents=discord.Intents.default())
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
-bot = Bot(intents=intents, command_prefix='$') # or whatever prefix you choose(!,%,?)
+    guild = discord.utils.get(client.guilds, name='Skill Brigade')
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
 
+
+bot = Bot(intents=intents, command_prefix='$')  # command prefix
+
+#keep_alive()
+print(os.getenv('TOKEN') is None)
 client.run(TOKEN)
